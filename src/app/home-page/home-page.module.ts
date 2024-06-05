063a92e3ff9dea3b76092a56817cb76b15d77ba0
Morcado <gposcarr18@gmail.com>
2024-06-05 14:03:14 -0600

    add principal sections of page

--- src/app/home-page/home-page.module.ts
+++ src/app/home-page/home-page.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page.component';
 import { MaterialModule } from '../material/material.module';
 import { ComponentsModule } from '../components/components.module';
+import { NormativaComponent } from './normativa/normativa.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
+    NormativaComponent,
+    AcercaDeComponent,
+    PrincipalComponent,
   ],
   imports: [
     CommonModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
-
+import {MatListModule} from '@angular/material/list';
 @NgModule({
   exports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatListModule,
   ]
 })
 export class MaterialModule { }
