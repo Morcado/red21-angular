4bb90522ddce21c822f9cfd6fdcbf50cf279df91
Morcado <gposcarr18@gmail.com>
2024-06-11 10:55:02 -0600

    changed main circle image

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatListModule} from '@angular/material/list';
+import {MatCardModule} from '@angular/material/card';
 @NgModule({
   exports: [
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatListModule,
+    MatCardModule,
   ]
 })
 export class MaterialModule { }
