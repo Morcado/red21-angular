06b4abbd4b385ab10cb7a9c7364cdf47ce57af86
Morcado <gposcarr18@gmail.com>
2024-06-12 11:45:18 -0600

    added component for download button

---
+++ src/app/home-page/normativa/document-button/document-button.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-document-button',
+  templateUrl: './document-button.component.html',
+  styleUrls: ['./document-button.component.css']
+})
+export class DocumentButtonComponent implements OnInit {
+  @Input()
+  public name: string;
+
+  @Input()
+  public title: number;
+
+  constructor() {
+    this.name = "";
+    this.title = 0;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
